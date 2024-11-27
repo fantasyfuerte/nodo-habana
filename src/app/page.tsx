9e7e3fd9405f353832b5a8822b60473ba46db2ca
fuerte <leandrofuerte75@gmail.com>
2024-11-27 06:25:00 +0100

    Adding logo image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1>Nodo Habana</h1>
-      <p>Nodo habana webpage</p>
+    <div className="flex justify-center h-96 -mt-40">
+      <Image
+        src="/logo.jpg"
+        width={550}
+        height={200}
+        alt="logo image"
+        className="object-cover object-top"
+      />
     </div>
   );
 }
