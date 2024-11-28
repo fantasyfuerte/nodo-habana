2d9455d3099896dfe1b1045250b10f37037b085e
fuerte <leandrofuerte75@gmail.com>
2024-11-28 16:48:25 +0100

    Creating thr routes and styling the nav

--- src/app/components/nav-global.tsx
+++ src/app/components/nav-global.tsx
@@ -1,23 +1,34 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Nav() {
+  const path = usePathname();
+
   return (
     <div className="flex justify-center gap-1 mt-2 ">
       <Link
         href="/shows"
-        className="w-20 font-semibold hover:opacity-70 hover:scale-105 transition text-center"
+        className={`w-20 font-semibold hover:opacity-70 hover:scale-105 transition text-center ${
+          path.includes("/shows") && "text-indigo-950"
+        }`}
       >
         Shows
       </Link>
       <Link
         href="/"
-        className="w-20 border-x-2 border-sky-200 font-semibold hover:opacity-70 hover:scale-105 transition text-center"
+        className={`w-20 border-x-2 border-sky-200 font-semibold hover:opacity-70 hover:scale-105 transition text-center ${
+          path == "/" && "text-indigo-950"
+        }`}
       >
         Inicio
       </Link>
       <Link
         href="/menu"
-        className="w-20 font-semibold hover:opacity-70 hover:scale-105 transition text-center"
+        className={`font- w-20 font-semibold hover:opacity-70 hover:scale-105 transition text-center ${
+          path.includes("/menu") && "text-indigo-950"
+        }`}
       >
         Menu
       </Link>
