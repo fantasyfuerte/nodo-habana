ad28fe9035e91e39439e4175e189ee92f972668d
fuerte <leandrofuerte75@gmail.com>
2024-11-28 16:58:48 +0100

    Creating not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="grid place-content-center font-bold h-[50vh]">
+      <h6 className="text-3xl text-red-950">Not found</h6>
+    </div>
+  );
+}
