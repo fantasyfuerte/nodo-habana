a957cabca683bee9b40660fb5ad014598377dd22
fuerte <leandrofuerte75@gmail.com>
2024-12-02 16:28:32 +0100

    Turning StyledArticle in SSR

--- src/app/components/StyledArticle.tsx
+++ src/app/components/StyledArticle.tsx
@@ -1,13 +1,10 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { useState } from "react";
+import Image from "next/image";
 
 interface Props {
   title: string;
   subtitle: string;
   paragraph: string;
-  imgs: string[];
+  img: string;
   centerImg?: boolean;
   id?: string;
 }
@@ -16,25 +13,12 @@ const StyledArticle: React.FC<Props> = ({
   title,
   subtitle,
   paragraph,
-  imgs,
+  img,
   centerImg,
   id,
 }) => {
-  const [img, setImg] = useState(0);
-  const changeImg = () => {
-    setImg((prev) => {
-      if (prev == imgs.length - 1) return 0;
-      return prev + 1;
-    });
-  };
   return (
-    <motion.article
-      id={id}
-      transition={{ delay: 0.5, duration: 0.2 }}
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      className="flex max-h-max flex-wrap items-end mb-48 py-32"
-    >
+    <article id={id} className="flex max-h-max flex-wrap items-end mb-48 mt-32">
       <div className="basis-6/12 flex-grow  pr-4 md:pr-0 pl-4 mb-7 ">
         <h5 className="text-4xl min-w-max md:text-5xl">
           {title}: <br></br>
@@ -45,15 +29,17 @@ const StyledArticle: React.FC<Props> = ({
         </p>
       </div>
       <div className="basis-5/12 flex-grow h-96 px-2 md:p-5 pb-0">
-        <motion.img
-          onViewportEnter={changeImg}
-          src={imgs[img]}
+        <Image
+          alt="imagen del articulo"
+          src={img}
           className={`object-cover ${
             !centerImg && "object-bottom"
           } w-full h-full rounded-xl`}
+          width={720}
+          height={360}
         />
       </div>
-    </motion.article>
+    </article>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         title="Nuestro centro"
         subtitle="Un lugar mágico"
         paragraph="En el corazón del Malecón habanero, Nodo Habana se erige como un espacio vibrante donde la tradición y la innovación se entrelazan. Este proyecto de desarrollo local no solo revitaliza el entorno urbano, sino que también fomenta la colaboración comunitaria, impulsando el crecimiento cultural y económico de la zona. Aquí, cada rincón cuenta una historia, y cada iniciativa es una chispa que enciende el espíritu creativo de La Habana. Nodo Habana es más que un centro, es un punto de encuentro donde las ideas cobran vida y la magia de la ciudad se renueva día a día."
-        imgs={["balcon.jpeg"]}
+        img={"/balcon.jpeg"}
       />
     </section>
   );
