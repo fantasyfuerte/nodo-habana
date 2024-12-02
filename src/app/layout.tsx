dd290fc619e2e3ea2fb9bcb8fe8e05a4c4790c7d
fuerte <leandrofuerte75@gmail.com>
2024-12-02 20:21:05 +0100

    Restyling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
         <footer className="bg-white rounded-lg shadow m-4">
           <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span className="text-sm text-gray-500 sm:text-center">
-              © 2024{" "}
-              <a href="https://flowbite.com/" className="hover:underline">
+              © 2024
+              <a href="#" className="hover:underline">
                 Nodo Habana™
               </a>
               . All Rights Reserved.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,37 +2,40 @@ import StyledArticle from "./components/StyledArticle";
 
 export default function Home() {
   return (
-    <section className="">
-      <article className="shadow-custom pb-16 mb-36">
-        <h1 className="text-center text-2xl font-semibold py-8 mt-10">
-          Un espacio de conexiones culturales
-        </h1>
-        <p className="text-center px-14 text-lg text-gray-800 first-letter:ml-10">
-          Bienvenido a Nodo Habana, un espacio dedicado a la promoción cultural
-          en el Malecón, donde el arte, la creatividad y la colaboración
-          <br></br> se encuentran para construir nuevas oportunidades. Nuestro
-          propósito es ser un punto de encuentro para artistas, creadores y
-          emprendedores,<br></br>
-          impulsando el talento local y ofreciendo una plataforma accesible y
-          dinámica donde la creatividad se desarrolla y toma forma. En Nodo
-          Habana, creemos en el poder de la cultura como motor de transformación
-          social, combinando tradición y modernidad para generar experiencias
-          significativas.<br></br> Ya sea a través de exposiciones, talleres,
-          eventos o proyectos comunitarios, buscamos conectar a personas
-          apasionadas que desean contribuir activamente al crecimiento cultural
-          de nuestra ciudad.
-          <br></br>
-          <strong className="text-[19px]">
-            Únete y sé parte del latido que impulsa La Habana.
-          </strong>
-        </p>
-      </article>
-      <StyledArticle
-        title="Nuestro centro"
-        subtitle="Un lugar mágico"
-        paragraph="En el corazón del Malecón habanero, Nodo Habana se erige como un espacio vibrante donde la tradición y la innovación se entrelazan. Este proyecto de desarrollo local no solo revitaliza el entorno urbano, sino que también fomenta la colaboración comunitaria, impulsando el crecimiento cultural y económico de la zona. Aquí, cada rincón cuenta una historia, y cada iniciativa es una chispa que enciende el espíritu creativo de La Habana. Nodo Habana es más que un centro, es un punto de encuentro donde las ideas cobran vida y la magia de la ciudad se renueva día a día."
-        img={"/balcon.jpeg"}
-      />
-    </section>
+    <>
+      <section className="">
+        <article className="shadow-custom pb-16 mb-36">
+          <h1 className="text-center text-2xl font-semibold py-8 mt-10">
+            Un espacio de conexiones culturales
+          </h1>
+          <p className="text-center px-14 text-md md:text-lg text-gray-800 first-letter:ml-10">
+            Bienvenido a Nodo Habana, un espacio dedicado a la promoción
+            cultural en el Malecón, donde el arte, la creatividad y la
+            colaboración
+            <br></br> se encuentran para construir nuevas oportunidades. Nuestro
+            propósito es ser un punto de encuentro para artistas, creadores y
+            emprendedores,<br></br>
+            impulsando el talento local y ofreciendo una plataforma accesible y
+            dinámica donde la creatividad se desarrolla y toma forma. En Nodo
+            Habana, creemos en el poder de la cultura como motor de
+            transformación social, combinando tradición y modernidad para
+            generar experiencias significativas.
+            <br></br>
+            <strong className="md:text-[19px]">
+              Únete y sé parte del latido que impulsa La Habana.
+            </strong>
+          </p>
+        </article>
+      </section>
+      <div className="w-full h-14 bg-gradient-to-t from-blue-600"></div>
+      <section className="bg-blue-600">
+        <StyledArticle
+          title="Nuestro centro"
+          subtitle="Un lugar mágico"
+          paragraph="En el corazón del Malecón habanero, Nodo Habana se erige como un espacio vibrante donde la tradición y la innovación se entrelazan. Este proyecto de desarrollo local no solo revitaliza el entorno urbano, sino que también fomenta la colaboración comunitaria, impulsando el crecimiento cultural y económico de la zona. Aquí, cada rincón cuenta una historia, y cada iniciativa es una chispa que enciende el espíritu creativo de La Habana. Nodo Habana es más que un centro, es un punto de encuentro donde las ideas cobran vida y la magia de la ciudad se renueva día a día."
+          img={"/balcon.jpeg"}
+        />
+      </section>
+    </>
   );
 }
