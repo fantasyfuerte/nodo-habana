3507bca1d2c78e3864064198cfd8b728d1e536a9
fuerte <leandrofuerte75@gmail.com>
2024-12-02 11:00:49 +0100

    Styling paragraph and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,30 @@
 export default function Home() {
   return (
-    <section className="h-screen">
-      <h1 className="text-center text-2xl font-semibold py-11">
-        Un espacio de conexiones culturales
-      </h1>
-      <p>
-        Bienvenido a Nodo Habana, un espacio dedicado a la promoción cultural en
-        el Malecón, donde el arte, la creatividad y la colaboración se
-        encuentran para construir nuevas oportunidades. Nuestro propósito es ser
-        un punto de encuentro para artistas, creadores y emprendedores,
-        impulsando el talento local y ofreciendo una plataforma accesible y
-        dinámica donde la creatividad se desarrolla y toma forma. En Nodo
-        Habana, creemos en el poder de la cultura como motor de transformación
-        social, combinando tradición y modernidad para generar experiencias
-        significativas. Ya sea a través de exposiciones, talleres, eventos o
-        proyectos comunitarios, buscamos conectar a personas apasionadas que
-        desean contribuir activamente al crecimiento cultural de nuestra ciudad.
-        Únete y sé parte del latido que impulsa La Habana.
-      </p>
+    <section className="">
+      <article className="shadow-2xl pb-16">
+        <h1 className="text-center text-2xl font-semibold py-8 mt-10">
+          Un espacio de conexiones culturales
+        </h1>
+        <p className="text-center px-14 text-lg text-gray-800 first-letter:ml-10">
+          Bienvenido a Nodo Habana, un espacio dedicado a la promoción cultural
+          en el Malecón, donde el arte, la creatividad y la colaboración
+          <br></br> se encuentran para construir nuevas oportunidades. Nuestro
+          propósito es ser un punto de encuentro para artistas, creadores y
+          emprendedores,<br></br>
+          impulsando el talento local y ofreciendo una plataforma accesible y
+          dinámica donde la creatividad se desarrolla y toma forma. En Nodo
+          Habana, creemos en el poder de la cultura como motor de transformación
+          social, combinando tradición y modernidad para generar experiencias
+          significativas.<br></br> Ya sea a través de exposiciones, talleres,
+          eventos o proyectos comunitarios, buscamos conectar a personas
+          apasionadas que desean contribuir activamente al crecimiento cultural
+          de nuestra ciudad.
+          <br></br>
+          <strong className="text-[19px]">
+            Únete y sé parte del latido que impulsa La Habana.
+          </strong>
+        </p>
+      </article>
     </section>
   );
 }
