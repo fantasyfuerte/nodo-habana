0f4e1b77b3282cc8b3a6230ef739307825442d79
fuerte <leandrofuerte75@gmail.com>
2024-12-05 12:11:36 +0100

    Refactoring HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import ArticlesList from "./components/ArticlesList";
 export default function Home() {
   return (
     <>
-      <section className="">
-        <article className="pb-16 mb-36">
-          <h1 className="text-center text-2xl font-semibold py-8 mt-10">
-            Un espacio de conexiones culturales
-          </h1>
+      <section>
+        <h1 className="text-center text-2xl font-semibold mb-16 py-5">
+          Un espacio de conexiones culturales
+        </h1>
+        <article className="mb-36 py-5">
+          <h3 className="text-center text-xl font-semibold mb-3">
+            ¿Quiénes somos?
+          </h3>
           <p className="text-center px-14 text-md md:text-lg text-gray-800 first-letter:ml-10">
             Bienvenido a Nodo Habana, un espacio dedicado a la promoción
             cultural en el Malecón, donde el arte, la creatividad y la
