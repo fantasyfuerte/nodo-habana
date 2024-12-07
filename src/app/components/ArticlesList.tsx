deb2c6d5d18faf9b884a1fa8ca167a50bbdb71ed
Leandro Fuerte <leandrofuerte75@gmail.com>
2024-12-07 07:31:18 -0500

    Writting second paragraph

--- src/app/components/ArticlesList.tsx
+++ src/app/components/ArticlesList.tsx
@@ -10,10 +10,10 @@ const principalArticles = [
     centerImg: true,
   },
   {
-    title: "Nuestro centro",
-    subtitle: "Un lugar mágico",
+    title: "Escena cultural",
+    subtitle: "Aires de mar y arte",
     paragraph:
-      "En el corazón del Malecón habanero, Nodo Habana se erige como un espacio vibrante donde la tradición y la innovación se entrelazan. Este proyecto de desarrollo local no solo revitaliza el entorno urbano, sino que también fomenta la colaboración comunitaria, impulsando el crecimiento cultural y económico de la zona. Aquí, cada rincón cuenta una historia, y cada iniciativa es una chispa que enciende el espíritu creativo de La Habana. Nodo Habana es más que un centro, es un punto de encuentro donde las ideas cobran vida y la magia de la ciudad se renueva día a día.",
+      "Situado en el icónico Malecón habanero, este teatro-bar ofrece una experiencia inmersiva en la cultura cubana. Más que un simple lugar para beber, es un espacio donde el arte, la música y la historia se fusionan en un ambiente vibrante y relajado. Desde conciertos espontáneos hasta exposiciones artísticas y noches de teatro, cada visita promete una aventura diferente, siempre con el encanto inconfundible de la brisa marina y la energía de La Habana como telón de fondo.",
     img: "/balcon.jpeg",
     centerImg: true,
   },
