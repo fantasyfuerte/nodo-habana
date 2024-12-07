82aece600ebe4c5d903906dacfcd406d7ac9ed6c
Leandro Fuerte <leandrofuerte75@gmail.com>
2024-12-07 07:43:16 -0500

    Changing Link by anchor element and puttin urls

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -1,6 +1,6 @@
 const links = [
-  { img: "/social/instagram.svg", url: "", alt: "Instagram logo" },
-  { img: "/social/facebook.svg", url: "", alt: "Facebook logo" },
+  { img: "/social/instagram.svg", url: "https://www.instagram.com/nodo.habana", alt: "Instagram logo" },
+  { img: "/social/facebook.svg", url: "https://www.facebook.com/share/kpYVar11M26NXYz7", alt: "Facebook logo" },
 ];
 import Link from "next/link";
 
@@ -10,9 +10,9 @@ export default function SocialLinks() {
       <ul className="flex gap-2 justify-center">
         {links.map((link, index) => (
           <li key={index} className="hover:opacity-65 transition-opacity">
-            <Link href={link.url}>
+            <a href={link.url}>
               <img alt={link.alt} width={20} height={20} src={link.img} />
-            </Link>
+            </a>
           </li>
         ))}
       </ul>
