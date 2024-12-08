fda03b2331de50bff3779a712222d2a7f91abdfd
fuerte <leandrofuerte75@gmail.com>
2024-12-08 19:16:16 +0100

    Changing text design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,14 @@ export default function Home() {
           <h3 className="text-center text-xl font-semibold mb-3">
             ¿Quiénes somos?
           </h3>
-          <p className="text-center px-2 md:px-14 text-md md:text-lg text-gray-800 first-letter:ml-10">
-            Bienvenido a Nodo Habana, un espacio dedicado a la promoción
+          <p className="text-center px-2 md:px-44 text-md md:text-lg text-gray-800 first-letter:ml-10">
+            Bienvenido a Nodo Habana,<br></br> un espacio dedicado a la promoción
             cultural en el Malecón, donde el arte, la creatividad y la
-            colaboración
-            <br></br> se encuentran para construir nuevas oportunidades. Nuestro
-            propósito es ser un punto de encuentro para artistas, creadores y
-            emprendedores,<br></br>
-            impulsando el talento local y ofreciendo una plataforma accesible y
-            dinámica donde la creatividad se desarrolla y toma forma.
+            colaboración se encuentran para construir nuevas oportunidades.
+            Nuestro propósito es ser un punto de encuentro para
+            artistas, creadores y impulsando el talento local y ofreciendo una
+            plataforma accesible y dinámica donde la creatividad se desarrolla y
+            toma forma.<br></br>
             <br></br>
             <strong className="md:text-[19px]">
               Únete y sé parte del latido que impulsa La Habana.
