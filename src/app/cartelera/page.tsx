036c97df60158388d62079ecfa4d9795a046d0f2
fuerte <leandrofuerte75@gmail.com>
2024-12-12 06:53:48 +0100

    Adding another Event

--- src/app/cartelera/page.tsx
+++ src/app/cartelera/page.tsx
@@ -7,6 +7,14 @@ const eventArticles = [
     date: "Lunes 16 de Diciembre",
     img: "/fachada.jpeg",
   },
+  {
+    title: "Apertura cultural del ejemplo",
+    paragraph:
+      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
+    time: ["8:00PM", "11:00PM"],
+    date: "Lunes 16 de Diciembre",
+    img: "/fachada.jpeg",
+  },
 ];
 import EventArticle from "./ui/Event";
 
