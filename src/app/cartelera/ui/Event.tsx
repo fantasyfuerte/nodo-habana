437c096d17bfa451e1436abe0059e96bbeee187e
fuerte <leandrofuerte75@gmail.com>
2024-12-12 07:09:41 +0100

    Finishing img styling

--- src/app/cartelera/ui/Event.tsx
+++ src/app/cartelera/ui/Event.tsx
@@ -10,8 +10,8 @@ interface Props {
 
 export default function Event({ title, paragraph, time, date, img }: Props) {
   return (
-    <article className="flex justify-between p-2 w-[45%] select-none">
-      <div className="text-[19px] basis-2/3">
+    <article className="flex justify-between p-2 max-w-[45%] select-none">
+      <div className="text-[18.5px] basis-[60%]">
         <h3 className="font-semibold">{title}</h3>
         <p className="font-normal text-gray-600">{paragraph}</p>
         <ul className="mt-5">
@@ -27,14 +27,14 @@ export default function Event({ title, paragraph, time, date, img }: Props) {
           </li>
         </ul>
       </div>
-      <div className="basis-1/3 object-cover">
+      <div className="basis-[40%] h-[187px]">
         <Image
-          className="rounded-md w-full"
-          width={170}
-          height={160}
+          className="rounded-md w-full h-full object-cover"
+          width={390}
+          height={350}
           src={img}
           alt="Imagen del articulo"
-        ></Image>
+        />
       </div>
     </article>
   );
