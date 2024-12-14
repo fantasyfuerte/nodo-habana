07f14406e7d49e94f8e82a1de59f904b6c20bc63
fuerte <leandrofuerte75@gmail.com>
2024-12-14 10:28:04 +0100

    Improving Responsive

--- src/app/cartelera/ui/EventsSection.tsx
+++ src/app/cartelera/ui/EventsSection.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function EventsSection({ events }: Props) {
   return (
-    <section className="grid grid-cols-[repeat(2,1fr)] py-4 px-6">
+    <section className="flex flex-wrap  lg:grid md:grid-cols-[repeat(2,minmax(500px,1fr))] py-4 md:px-6">
       {events.map((event, index) => (
         <EventArticle {...event} key={index} />
       ))}
