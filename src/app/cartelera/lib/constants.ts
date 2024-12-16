33c6404c91d2e4eb5e3e3b2406c09fafc926a075
fuerte <leandrofuerte75@gmail.com>
2024-12-16 17:02:48 +0100

    Changing dates

--- src/app/cartelera/lib/constants.ts
+++ src/app/cartelera/lib/constants.ts
@@ -4,6 +4,30 @@ export const eventArticles = [
     paragraph:
       "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
     time: ["8:00PM", "11:00PM"],
+    date: new Date(2024, 11, 13),
+    img: "/fachada.jpeg",
+  },
+  {
+    title: "Apertura cultural del ejemplo",
+    paragraph:
+      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
+    time: ["8:00PM", "11:00PM"],
+    date: new Date(2024, 11, 14),
+    img: "/balcon.jpeg",
+  },
+  {
+    title: "Apertura cultural del ejemplo",
+    paragraph:
+      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
+    time: ["8:00PM", "11:00PM"],
+    date: new Date(2024, 11, 15),
+    img: "/balcon.jpeg",
+  },
+  {
+    title: "Apertura cultural del ejemplo",
+    paragraph:
+      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
+    time: ["8:00PM", "11:00PM"],
     date: new Date(2024, 11, 16),
     img: "/fachada.jpeg",
   },
@@ -12,7 +36,7 @@ export const eventArticles = [
     paragraph:
       "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
     time: ["8:00PM", "11:00PM"],
-    date: new Date(2024, 11, 16),
+    date: new Date(2024, 11, 17),
     img: "/balcon.jpeg",
   },
   {
@@ -20,31 +44,7 @@ export const eventArticles = [
     paragraph:
       "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
     time: ["8:00PM", "11:00PM"],
-    date: new Date(2024, 11, 16),
-    img: "/balcon.jpeg",
-  },
-  {
-    title: "Apertura cultural del ejemplo",
-    paragraph:
-      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
-    time: ["8:00PM", "11:00PM"],
-    date: new Date(2024, 11, 16),
-    img: "/fachada.jpeg",
-  },
-  {
-    title: "Apertura cultural del ejemplo",
-    paragraph:
-      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
-    time: ["8:00PM", "11:00PM"],
-    date: new Date(2024, 11, 16),
-    img: "/balcon.jpeg",
-  },
-  {
-    title: "Apertura cultural del ejemplo",
-    paragraph:
-      "Parrafo de ejemplo con poca importancia visual. Aqui se explica el evento en menos de tres líneas.",
-    time: ["8:00PM", "11:00PM"],
-    date: new Date(2024, 11, 16),
+    date: new Date(2024, 11, 18),
     img: "/balcon.jpeg",
   },
 ];
