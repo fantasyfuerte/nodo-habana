e6f5d0da5e35f803db93ca78d18561e331036eb0
fuerte <leandrofuerte75@gmail.com>
2024-12-17 10:59:29 +0100

    Puttin relativeDates

--- src/app/cartelera/page.tsx
+++ src/app/cartelera/page.tsx
@@ -1,8 +1,6 @@
 import EventsSection from "./ui/EventsSection";
 import { eventArticles } from "@/app/cartelera/lib/constants";
 
-const today = new Date();
-
 export default function Page() {
   return (
     <main className="select-none">
@@ -12,18 +10,7 @@ export default function Page() {
       >
         Próximamente
       </h1>
-      <EventsSection
-        events={eventArticles.filter((event) => event.date > today)}
-      />
-      <h1
-        className="text-2xl font-semibold text-center py-5"
-        id="ultimos-eventos"
-      >
-        Últimos eventos
-      </h1>
-      <EventsSection
-        events={eventArticles.filter((event) => event.date < today)}
-      />
+      <EventsSection events={eventArticles} />
     </main>
   );
 }
