dc247b53cc63de3e0d8a0fcd15965d6a5c9e7a69
fuerte <leandrofuerte75@gmail.com>
2024-12-20 09:15:00 +0100

    Enhance TodayEvent component with improved styling for date and time display

--- src/app/ui/TodayEvent.tsx
+++ src/app/ui/TodayEvent.tsx
@@ -35,8 +35,8 @@ const TodayEvent = ({ event }:Props) => {
       </h2>
       <article className="bg-white p-6 rounded-lg shadow-lg">
         <Image
-          width={700}
-          height={700}
+          width={1000}
+          height={1000}
           src={event.img}
           alt={event.title}
           className="w-full h-64 object-cover rounded-t-lg mb-4"
@@ -45,10 +45,10 @@ const TodayEvent = ({ event }:Props) => {
           <h3 className="text-xl font-bold mb-2">{event.title}</h3>
           <p className="text-gray-700 mb-4">{event.paragraph}</p>
           <p className="text-gray-500">
-            Fecha: {getRelativeDate(event.date)}
+            Fecha: <strong className="text-black">{getRelativeDate(event.date)}</strong>
           </p>
           <p className="text-gray-500">
-            Hora: <strong>{event.time[0]} - {event.time[1]}</strong>
+            Hora: <strong className="text-black">{event.time[0]} - {event.time[1]}</strong>
           </p>
         </div>
       </article>
