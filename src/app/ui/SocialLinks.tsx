6f3f78533cdb284ecb7bdd7fcb64917729f8db41
fuerte <leandrofuerte75@gmail.com>
2024-12-25 19:38:09 +0100

    Adding target="_blank" in anchor element

--- src/app/ui/SocialLinks.tsx
+++ src/app/ui/SocialLinks.tsx
@@ -1,6 +1,14 @@
 const links = [
-  { img: "/social/instagram.svg", url: "https://www.instagram.com/nodo.habana", alt: "Instagram logo" },
-  { img: "/social/facebook.svg", url: "https://www.facebook.com/share/kpYVar11M26NXYz7", alt: "Facebook logo" },
+  {
+    img: "/social/instagram.svg",
+    url: "https://www.instagram.com/nodo.habana",
+    alt: "Instagram logo",
+  },
+  {
+    img: "/social/facebook.svg",
+    url: "https://www.facebook.com/share/kpYVar11M26NXYz7",
+    alt: "Facebook logo",
+  },
 ];
 
 export default function SocialLinks() {
@@ -9,7 +17,7 @@ export default function SocialLinks() {
       <ul className="flex gap-2 justify-center">
         {links.map((link, index) => (
           <li key={index} className="hover:opacity-65 transition-opacity">
-            <a href={link.url}>
+            <a href={link.url} target="_blank">
               <img alt={link.alt} width={20} height={20} src={link.img} />
             </a>
           </li>
